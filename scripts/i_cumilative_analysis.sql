17ea1495ef9708feeea51e49953b94d7addb89c9
Erabathini Shreesha <shreesha.erabathini@gmail.com>
2026-02-12 18:18:38 +0530

    Add cumulative analysis SQL script
    This SQL script calculates running totals and moving averages for sales metrics over time.

---
+++ scripts/i_cumilative_analysis.sql
@@ -0,0 +1,27 @@
+/*
+===============================================================================
+                        CUMULATIVE ANALYSIS
+     To calculate running totals or moving averages for key metrics and
+            To track performance over time cumulatively.
+===============================================================================
+
+SQL Functions Used:
+    - Window Functions: SUM, AVG, OVER
+===============================================================================
+*/
+---- Retrieving total years sales and cumilative growth over time ----
+SELECT *,
+	SUM(total_sales) OVER( ORDER BY order_Date) AS running_total,
+	AVG(average_price) OVER(ORDER BY order_Date) AS running_average
+FROM
+(
+SELECT 
+    DATETRUNC(YEAR,order_date) AS order_Date,
+    SUM(sales_amount) AS total_sales,
+    AVG(sales_amount) AS average_price
+FROM gold.fact_sales
+WHERE  DATETRUNC(YEAR,order_date)  IS NOT NULL
+GROUP BY DATETRUNC(YEAR,order_date) 
+)t
+
+
